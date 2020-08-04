2d4702a719a4e9e60299d8e9f11272c368e638de
marcouellet <marc.ouellet2@gmail.com>
2020-08-04 09:26:09 -0400

    more js to tsx

--- src/component/ToastUndo/index.tsx
+++ src/component/ToastUndo/index.tsx
@@ -5,24 +5,31 @@ import { addDeletedWorkExperienceItem, addDeletedEducationItem, addDeletedSkillI
 
 import styles from './toastUndo.module.scss';
 
-const ToastUndo = ({ itemId, message, closeToast, data, type }) => {
-    const deletedItem = data.filter(({ id }) => id === itemId);
-    // const findIndex = data.findIndex(e => e.id === itemId);
+interface TProps {
+    itemId: string;
+    message: string;
+    data: {
+        [key: string]: string;
+    }[];
+    type: string;
+}
+
+const ToastUndo = (props: TProps) => {
+    const deletedItem = props.data.filter(({ id }) => id === props.itemId);
+
     function handleClick() {
-        if (type === 'workExperience') {
+        if (props.type === 'workExperience') {
             appStore.dispatch(addDeletedWorkExperienceItem(deletedItem));
-        } else if (type === 'education') {
+        } else if (props.type === 'education') {
             appStore.dispatch(addDeletedEducationItem(deletedItem));
-        } else if (type === 'skills') {
+        } else if (props.type === 'skills') {
             appStore.dispatch(addDeletedSkillItem(deletedItem));
         }
-
-        closeToast();
     }
 
     return (
         <div className={styles.toastBox}>
-            <div className={styles.toastMessage}>{message}</div>
+            <div className={styles.toastMessage}>{props.message}</div>
             <div className={styles.toastUndoBtn} onClick={handleClick}>
                 UNDO
             </div>
